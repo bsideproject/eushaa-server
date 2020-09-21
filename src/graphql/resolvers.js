3740eef846e56ec88c0754726d76e94fca1c72aa
sooob <dltntnqo@naver.com>
2020-09-21 23:03:33 +0900

    FIX] 줄맞춤

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3,7 +3,7 @@ const { insertUser, selectUser, autheticate } = require('../model/user')
 const resolvers = {
     Query: {
 	    login: (_, { email, password }) => selectUser(email, password),
-		isLogin: (_, { authorization }) => autheticate(authorization),
+	    isLogin: (_, { authorization }) => autheticate(authorization),
     },
     Mutation: {
         signup: (_, { email, name, password }) => insertUser(email, name, password)
