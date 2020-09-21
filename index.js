817dd28b6cce07de99276849b8de6b99dd651f3c
sohyeong-dev <so-hyeong@naver.com>
2020-09-21 05:09:19 +0000

    ADD: 로그인 - 토큰 기반 인증

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
+const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, 'config/.env') })
 
 const sequelize = require('./db/models/index').sequelize;
 const { GraphQLServer } = require('graphql-yoga');
 
-const user = require("./src/model/user")
+const resolvers = require('./src/graphql/resolvers')
 
-const typeDefs = `
-  type Mutation  {
-    signup(email: String! ,name: String!, password: String!): User
-  }
-  type User {
-    email: String!,
-    name: String!,
-    password: String!
-  }
-`;
-const resolvers = {
-    Query: {
-
-    },
-    Mutation: {
-        signup: (_, { email, name, password }) => user.insertUser(email, name, password)
-    }
-};
+const options = {
+  port: 3000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
 
 sequelize.sync();
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log('Server is running on localhost:4000'));
+const server = new GraphQLServer({ 
+	typeDefs: "./src/graphql/schema.graphql", 
+	resolvers });
+server.start(options, ({ port }) => console.log(`Server is running on learn-express.run.goorm.io:${port}`));
--- src/model/user/index.js
+++ src/model/user/index.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const { User } = require('../../../db/models');
 
@@ -14,4 +17,35 @@ exports.insertUser = async (email, name, password) => {
         password: hashedPassword
     })
     return user;
+}
+
+exports.selectUser = async (email, password) => {
+	const user = await User.findOne({
+		where: {
+			email,
+		}
+	})
+	if (user) {
+		if (await bcrypt.compareSync(password, user.password)) {
+			const token = jwt.sign({ id: user.id, email: user.email, name: user.name }, process.env.JWT_SECRET, {
+				algorithm: 'HS256',
+				expiresIn: '1h',
+			})
+			return token
+		}
+		throw new Error('Incorrect password.')
+	}
+	throw new Error('No Such User exists.')
+} 
+
+exports.autheticate = (authorization) => {
+	try {
+		const token = jwt.verify(authorization, process.env.JWT_SECRET)
+		if (token) {
+			return token.name
+		}
+		throw new Error('unauthorized.')
+	} catch (err) {
+		throw new Error('token expired')
+	}
 }
