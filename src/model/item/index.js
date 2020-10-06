6b52117f999873efbbf8065651b2d41c80880e54
suuub <dltntnqo@gmail.com>
2020-10-07 01:15:14 +0900

    ADD] item insert

--- src/model/item/index.js
+++ src/model/item/index.js
@@ -1,1 +1,14 @@
 const { Items } = require('../../../db/models');
+
+const { styleHyphenFormat } = require('../../lib/util')
+
+exports.insert = async ({ todoId, content }) => {
+
+    const item = await Items.create({
+        todo_id: todoId,
+        content
+    })
+    return item;
+}
+
+
