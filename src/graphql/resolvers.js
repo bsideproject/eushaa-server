853db56f9466c08dbecba98bd24051f2ac05a004
suuub <dltntnqo@gmail.com>
2020-10-07 01:17:12 +0900

    ADD] 리졸버 추가

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,12 +1,20 @@
 const { insertUser, selectUser, autheticate } = require('../model/user')
+const item = require('../model/item')
+const todo = require('../model/todo')
 
 const resolvers = {
     Query: {
-	    login: (_, { email, password }) => selectUser(email, password),
-	    isLogin: (_, { authorization }) => autheticate(authorization),
+        login: (_, { email, password }) => selectUser(email, password),
+        isLogin: (_, { authorization }) => autheticate(authorization),
     },
     Mutation: {
-        signup: (_, { email, name, password }) => insertUser(email, name, password)
+        signup: (_, { email, name, password }) => insertUser(email, name, password),
+
+        makeTodo: (_, { userId, title }) => todo.insert({ userId, title }),
+        updateTodo: (_, { id, userId, title }) => todo.update(id, { userId, title }),
+
+        makeItem: (_, { todoId, content }) => item.insert({ todoId, content }),
+        updateItem: (_, { id, todoId, content, isComplete, completedAt }) => item.update(id, { todoId, content, isComplete, completedAt }),
     }
 };
 
