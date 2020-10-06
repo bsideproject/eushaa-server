72f8ddd6692c4826912871b25c7d3fdba310e3e0
suuub <dltntnqo@gmail.com>
2020-10-07 01:14:13 +0900

    ADD] todo update

--- src/model/todo/index.js
+++ src/model/todo/index.js
@@ -1,5 +1,7 @@
 const { Todos } = require('../../../db/models');
 
+const { styleHyphenFormat } = require('../../lib/util')
+
 exports.insert = async ({ userId, title }) => {
 
     const todo = await Todos.create({
@@ -9,4 +11,13 @@ exports.insert = async ({ userId, title }) => {
     return todo;
 }
 
-
+exports.update = async (id, updateData) => {
+    updateData = Object.entries(updateData)
+        .map(([k, v]) => [styleHyphenFormat(k), v])
+        .reduce((acc, [k, v]) => {
+            acc[k] = v
+            return acc;
+        })
+    const todo = await Todos.update(updateData, { where: { id } })
+    return todo;
+}
