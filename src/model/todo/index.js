cc3b338367c7b1473ceadbea6120c9feb9f05978
suuub <dltntnqo@gmail.com>
2020-10-07 01:13:47 +0900

    ADD] todo insert

--- src/model/todo/index.js
+++ src/model/todo/index.js
@@ -1,1 +1,12 @@
 const { Todos } = require('../../../db/models');
+
+exports.insert = async ({ userId, title }) => {
+
+    const todo = await Todos.create({
+        user_id: userId,
+        title
+    })
+    return todo;
+}
+
+
