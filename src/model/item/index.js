fe20062c2f180ba2e6f47abe08720ae53ae81490
sohyeong-dev <so-hyeong@naver.com>
2020-10-07 05:35:11 +0000

    add: get items

--- src/model/item/index.js
+++ src/model/item/index.js
@@ -1,1 +1,14 @@
 const { Items } = require('../../../db/models');
+
+exports.getItems = async todo_id => {
+	try {
+		const items = await Items.findAll({
+			where: {
+				todo_id
+			}
+		})
+		return items;
+	} catch (err) {
+		console.error(err);
+	}
+}
