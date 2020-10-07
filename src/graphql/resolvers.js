4b7ea2a1816ca8e57533072ec043f6f5cff14e86
sohyeong-dev <so-hyeong@naver.com>
2020-10-07 05:41:58 +0000

    add: resolver

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,12 +1,30 @@
 const { insertUser, selectUser, autheticate } = require('../model/user')
+const item = require('../model/item')
+const todo = require('../model/todo')
 
 const resolvers = {
     Query: {
-	    login: (_, { email, password }) => selectUser(email, password),
-	    isLogin: (_, { authorization }) => autheticate(authorization),
+		login: (_, { email, password }) => selectUser(email, password),
+		isLogin: (_, { authorization }) => autheticate(authorization),
+
+		todos: (_, { user_id }) => todo.getTodos(user_id),
+		todo: (_, { user_id, title }) => todo.getTodo(user_id, title),
+
+		items: (_, { todo_id }) => item.getItems(todo_id),
+		todoLists: (_, { user_id, title }) => item.getTodoItems(user_id, title),
     },
     Mutation: {
-        signup: (_, { email, name, password }) => insertUser(email, name, password)
+		signup: (_, { email, name, password }) => insertUser(email, name, password),
+
+		deleteTodo: (_, { id }) => {
+			const result = todo.removeTodo(id)
+			if (result) {
+				item.removeItemsAll(id)
+			}
+			return result
+		},
+
+		deleteItem: (_, { id }) => item.removeItem(id),
     }
 };
 
