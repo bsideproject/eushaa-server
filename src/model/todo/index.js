98a148e48a250c8d2216bb48444853dd06cf90cf
sohyeong-dev <so-hyeong@naver.com>
2020-10-07 05:32:52 +0000

    add: get todo

--- src/model/todo/index.js
+++ src/model/todo/index.js
@@ -1,1 +1,28 @@
 const { Todos } = require('../../../db/models');
+
+exports.getTodos = async user_id => {
+	try {
+		const todos = await Todos.findAll({
+			where: {
+				user_id
+			}
+		})
+		return todos
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+exports.getTodo = async (user_id, title) => {
+	try {
+		const todo = await Todos.findOne({
+			where: {
+				user_id,
+				title
+			}
+		})
+		return todo
+	} catch (err) {
+		console.error(err)
+	}
+}
