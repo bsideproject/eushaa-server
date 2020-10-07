5a43648744169bfc83c686b7ad1ab30dbfa0ae79
sohyeong-dev <so-hyeong@naver.com>
2020-10-07 05:36:59 +0000

    add: remove item

--- src/model/item/index.js
+++ src/model/item/index.js
@@ -12,3 +12,29 @@ exports.getItems = async todo_id => {
 		console.error(err);
 	}
 }
+
+exports.removeItem = async id => {
+	try {
+		const result = await Items.destroy({
+			where: {
+				id
+			}
+		})
+		return result
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+exports.removeItemsAll = async todo_id => {
+	try {
+		const result = await Items.destroy({
+			where: {
+				todo_id
+			}
+		})
+		return result
+	} catch (err) {
+		console.error(err)
+	}
+}
