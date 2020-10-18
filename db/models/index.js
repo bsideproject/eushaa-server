bd60a302dec303de4c7f46827e19ebcd286315b7
soob-forest <dltntnqo@naver.com>
2020-10-19 00:47:14 +0900

    ADD] spaceItemLogs 모델 추가

--- db/models/index.js
+++ db/models/index.js
@@ -37,6 +37,7 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Todos = require('./todos')(sequelize, Sequelize);
 db.Items = require('./items')(sequelize, Sequelize);
+db.spaceItemLogs = require('./space_item_log')(sequelize, Sequelize);
 
 //db.Todos.hasMany(db.Items, { foreignKey: 'todo_id', sourceKey: 'id' });
 //db.Items.belongsTo(db.Todos, { foreignKey: 'todo_id', targetKey: 'id' });
