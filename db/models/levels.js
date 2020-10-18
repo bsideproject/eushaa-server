b0fcf26d3060edd8e04e8d9b0c42eeb2c6221fa3
sohyeong-dev <so-hyeong@naver.com>
2020-10-18 14:06:32 +0000

    add: 팀 관련 모델 정의

---
+++ db/models/levels.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class levels extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+			this.hasMany(models.space_items, { foreignKey: 'level_id', sourceKey: 'id' });
+    }
+  };
+  levels.init({
+    level: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+    bg_img: DataTypes.STRING(255),
+  }, {
+    timestamps: false,
+    sequelize,
+    modelName: 'levels',
+  });
+
+  return levels;
+};
