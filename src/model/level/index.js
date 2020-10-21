d2f17ce5f546ee002a953455deca0d5b103b108b
soob-forest <dltntnqo@naver.com>
2020-10-21 19:56:08 +0900

    FIX] await 함수 쓰는데 async 안씀

--- src/model/level/index.js
+++ src/model/level/index.js
@@ -1,6 +1,6 @@
 const { Levels } = require('../../../db/models');
 
-exports.get = ({ levelNumber }) => {
+exports.get = async({ levelNumber }) => {
     const level = await Levels.findOne({
         where: {
             level: levelNumber
