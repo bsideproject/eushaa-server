475a79373310f9bc26f65f50b9eab7aa835a9ebd
sohyeong-dev <so-hyeong@naver.com>
2020-11-11 15:45:26 +0000

    fix: 캐릭터 아이템 관계 추가

--- db/models/character_item_logs.js
+++ db/models/character_item_logs.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+        this.belongsTo(models.CharacterItems, { foreignKey: 'character_item_id', sourceKey: 'id' });
     }
   };
   characterItemLogs.init({
