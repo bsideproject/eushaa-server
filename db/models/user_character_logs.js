e641a7afd0d86ee9ffaaf732bc72f2af53770b9d
soob-forest <dltntnqo@naver.com>
2020-11-22 16:35:05 +0900

    FIX] character log 테이블명 변경

--- db/models/user_character_logs.js
+++ db/models/user_character_logs.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class characterLogs extends Model {
+  class userCharacterLogs extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,14 +13,14 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  characterLogs.init({
-    log: { type: DataTypes.TEXT, allowNull: false},
+  userCharacterLogs.init({
+    log: { type: DataTypes.TEXT, allowNull: false },
     created_at: { type: DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP') }
   }, {
     timestamps: false,
     sequelize,
-    modelName: 'character_logs',
+    modelName: 'user_character_logs',
   });
 
-  return characterLogs;
+  return userCharacterLogs;
 };
