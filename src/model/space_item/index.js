0e1c110ccd4181c82672ea28038c15be0db37e09
soob-forest <dltntnqo@naver.com>
2020-12-03 01:24:28 +0900

    FIX] 안쓰는거 삭제

--- src/model/space_item/index.js
+++ src/model/space_item/index.js
@@ -1,4 +1,3 @@
-const { where } = require('sequelize');
 const { SpaceItems, SpaceItemLog, Levels, User, Teams } = require('../../../db/models');
 
 const { styleUpperFormat } = require('../../lib/util');
